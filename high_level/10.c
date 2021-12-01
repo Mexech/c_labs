ed81c3682cf8f78526d23fa4ff048147bf931090
BuildTools <unconfigured@null.spigotmc.org>
2021-12-01 20:48:56 +0300

    10 ready

--- high_level/10.c
+++ high_level/10.c
@@ -1,3 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int is_delimiter(int c)
+{
+  return c == ' ' || c == '.' || c == ',' || c == '\r' || c == '\n';
+}
+
+// Вводится символ, которым заменяют разделители в файле
 void main() {
-  // Вводится символ, которым заменяют разделители в файле
+  FILE *in, *out;
+  char *replace_str;
+  char replace_c;
+  char c;
+  scanf("%s", replace_str);
+  if (!strcmp("tab", replace_str))
+    replace_c = '\t';
+  else if (!strcmp("enter", replace_str))
+    replace_c = '\n';
+  else
+    replace_c = replace_str[0];
+  in = fopen("F:/Projects/c_labs/high_level/input.txt", "r");
+  if (in == NULL)
+  {
+    perror("nonexistent input file");
+  }
+  out = fopen("F:/Projects/c_labs/high_level/output.txt", "w");
+  if (out == NULL)
+  {
+    perror("nonexistent input file");
+  }
+  while ((c = fgetc(in)) != EOF) {
+    if (is_delimiter(c))
+      fputc(replace_c, out);
+    else 
+      fputc(c, out);
+  }  
 }
