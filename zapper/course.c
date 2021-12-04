2a59329f3317f0f05ce4a536ec4dc469ba1f9b14
BuildTools <unconfigured@null.spigotmc.org>
2021-12-04 23:25:01 +0300

    started work on making dict dynamic

--- zapper/course.c
+++ zapper/course.c
@@ -2,8 +2,8 @@
 #include <string.h>
 
 #define BUFF_SIZE 8192
-#define DICT_SIZE 1024
-
+// #define INIT_DICT_SIZE 1024
+#define INIT_DICT_SIZE 270
 #define len(a)                      \
     ({int l;                        \
     for (l = 0; l < BUFF_SIZE; l++) \
@@ -11,29 +11,47 @@
             break;                  \
     l;})
 
-char *dict[DICT_SIZE];
 int dict_extended_len = 0;
+int dict_size = INIT_DICT_SIZE;
+
+char **dict;
 
 int in_dict(char *seq, char next_char);
 int dict_index(char *seq);
 void fill_dict(char *dict);
 int *lzw_encrypt(char *buff);
 char *lzw_decrypt(int *buff);
+void lzw_encrypt_file(char *path, char *to_archive);
+void lzw_decrypt_file(char *path);
 
 void main()
 {
-    FILE *in, *out;
-    in = fopen("in.txt", "rb");
-    if (in == NULL) {
-        perror("nonexistant input file");
-    }
-    out = fopen("out.zap", "wb+");
-    if (out == NULL) {
-        perror("nonexistant output file");
-    }
-    size_t n, m;
-    char buff[BUFF_SIZE] = {0};
-    int *message;
+    lzw_encrypt_file("in.txt", "out.zap");
+    // memset(message, 0, len(message)*sizeof(message[0]));
+    // fread(message, 4, BUFF_SIZE, in);
+    // memset(dict, 0, sizeof dict);
+    // memset(buff, 0, BUFF_SIZE);
+    // int i = 0;
+    // dict_extended_len = 0;
+    // for (i = 1; i < 256; i++)
+    // {
+    //     seq[0] = i;
+    //     char *seq_address = malloc(sizeof strlen(seq));
+    //     strcpy(seq_address, seq);
+    //     dict[i] = seq_address;
+    // }
+    // i = 0;
+    // memset(seq, 0, sizeof seq);
+
+    // char *result = lzw_decrypt(message);
+    // printf(result);
+
+    // if (m)
+    //     perror("copy");
+}
+
+void init_dict()
+{
     char seq[BUFF_SIZE] = {0};
     for (int i = 1; i < 256; i++)
     {
@@ -42,50 +60,40 @@ void main()
         strcpy(seq_address, seq);
         dict[i] = seq_address;
     }
-    do {
-        n = fread(buff, 1, sizeof buff, in);
-        int i_m = 0;
+}
+
+void lzw_encrypt_file(char *path, char *to_archive)
+{
+    char buff[BUFF_SIZE] = {0};
+    char seq[BUFF_SIZE] = {0};
+    dict = calloc(dict_size, sizeof(char *));
+    FILE *in = fopen(path, "rb");
+    if (in == NULL)
+        perror("nonexistant input file");
+    FILE *out = fopen(to_archive, "wb+"); // TODO: ab+ if length of paths array nonzero
+    if (out == NULL)
+        perror("nonexistant output file");
+    init_dict();
+    do
+    {
+        size_t n = fread(buff, 1, sizeof buff, in);
         memset(seq, 0, sizeof seq);
-        if (n) {
-            message = lzw_encrypt(buff);
+        if (n)
+        {
+            int *message = lzw_encrypt(buff);
+            // char *result = lzw_decrypt(message); //FIXME: breaks because of these string checks
+            // printf(result);
             fwrite(message, sizeof(message[0]), len(message), out);
         }
         else
-        {
-            m = 0;
-        }
-    } while ((n > 0) && (m == 1));
-    fclose(out);
-    in = fopen("out.zap", "rb");
-    if (in == NULL)
-    {
-        perror("nonexistant input file");
-    }
-    memset(message, 0, len(message)*sizeof(message[0]));
-    fread(message, 4, BUFF_SIZE, in);
-    memset(dict, 0, sizeof dict);
-    memset(buff, 0, BUFF_SIZE);
-    int i = 0;
-    dict_extended_len = 0;
-    for (i = 1; i < 256; i++)
-    {
-        seq[0] = i;
-        char *seq_address = malloc(sizeof strlen(seq));
-        strcpy(seq_address, seq);
-        dict[i] = seq_address;
-    }
-    i = 0;
-    memset(seq, 0, sizeof seq);
+            break;
+    } while (1);
+    dict_size = INIT_DICT_SIZE;
+}
 
-    char *result = lzw_decrypt(message);
-    printf(result);
+void lzw_decrypt_file(char *path)
+{
 
-    // if (m)
-    //     perror("copy");
-    if (fclose(out))
-        perror("close output file");
-    if (fclose(in))
-        perror("close input file");
 }
 
 int *lzw_encrypt(char *buff) {
@@ -105,7 +113,12 @@ int *lzw_encrypt(char *buff) {
                 seq[++j] = buff[i];
                 char *seq_address = malloc(sizeof strlen(seq));
                 strcpy(seq_address, seq);
+                if (256 + dict_extended_len + 1 >= dict_size) {
+                    dict_size *= 2;
+                    dict = realloc(dict, dict_size * sizeof(char *));
+                }
                 dict[256 + dict_extended_len++] = seq_address;
+                dict[256 + dict_extended_len] = NULL; // needed because after realloc memory is uninitialized
                 break;
             }
             else if (buff[i] == 0)
@@ -163,9 +176,13 @@ int in_dict(char *seq, char next_char)
 {
     int k;
     seq[strlen(seq)] = next_char;
+    // if (256 + dict_extended_len == 270) {
+
+    // }
     for (k = 256 + dict_extended_len; k >= 0; k--)
     {
-        if (!strcmp((dict[k] == 0) ? "" : dict[k], seq)){
+        if (!strcmp((dict[k] == 0) ? "" : dict[k], seq))
+        {
             return 1;
         }
     }
